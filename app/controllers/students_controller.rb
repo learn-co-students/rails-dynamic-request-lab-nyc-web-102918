81ec5807ceacf0f365663451f796709e80fd1fa5
vibhu mahendru <vibhumahendru@vibhus-MacBook-Pro.local>
2018-11-26 09:41:15 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,13 @@
 class StudentsController < ApplicationController
   def index
     @students = Student.all
+
+    # render 'index'
+  end
+
+
+
+  def show
+    @student = Student.first
   end
 end
